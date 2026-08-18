ba4bf24f021f065c4ee260f2d5a7dd83d7d4272f
Otávio Martineschen <otaviomart7tds@gmail.com>
2026-08-18 09:42:54 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Header(){
+    const [ menuAtivo, setMenuAtivo] = useState(false);
+
+    const toggleMenu = () =>{
+        setMenuAtivo(!menuAtivo);
+    };
+
+    return(
+        <>
+            <header className="header">
+                <h1 className="logo">Pizza FIEP</h1>
+                
+                {/* Quando clica, chama a função toggleMenu */}
+                <div className="meu-toggle" onClick={toggleMenu}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+
+                <nav className={`menu ${menuAtivo ? 'active' : ''}`}>
+                    <a href="#hero" onClick={toggleMenu}>Home</a>
+                    <a href="#cardapio" onClick={toggleMenu}>Cardápio</a>
+                    <a href="sobre" onClick={toggleMenu}>Sobre</a>
+                    <a href="#contato" onClick={toggleMenu}>Contato</a>
+                </nav>
+
+                <div className={`overlay-menu ${menuAtivo ? 'active' : ''}`} onClick={toggleMenu}>
+
+                </div>
+            </header>
+        </>
+    )
+}
+
