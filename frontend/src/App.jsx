12f4c6a827ab69297da6726da6209152e2e8eefb
Otávio Martineschen <otaviomart7tds@gmail.com>
2026-08-21 12:17:08 -0300

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Diferenciais from "./components/Diferenciais";
+import Cardapio from "./components/Cardapio";
+
+function App(){
+    return(
+        <>
+            <Header />
+            <Hero />
+            <Diferenciais />
+            <Cardapio />
+        </>
+    )
+}
+
+export default App;
